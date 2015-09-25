b23d0b7a1183f75a1b07420f6cf1968c69a36126
Alou Dolo <aloud7@gmail.com>
2015-09-25 21:28:42 +0000

    affichage dynamique des articles

--- kibaru/site/urls.py
+++ kibaru/site/urls.py
@@ -5,5 +5,7 @@ from kibaru.site import views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', views.home, name='home')
+                       url(r'^$', views.home, name='home'),
+                       url(r'^display_article/(?P<id>\d+)$', views.display_article, name='display_article'),
+                       url(r'^display_publicity/(?P<id>\d+)$', views.display_publicity, name='display_publicity'),
                        )
--- kibaru/site/views.py
+++ kibaru/site/views.py
@@ -1,7 +1,39 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+
+from kibaru.models import Article, Publicity
 
 
 def home(request):
+    context = {}
+    articles = Article.objects.all()
+    publicities = Publicity.objects.all()
+
+    for article in articles:
+        article.url_display = reverse("display_article", args=[article.id])
+
+    for publicity in publicities:
+        publicity.url_display = reverse("display_publicity", args=[publicity.id])
+    context.update({'articles': articles, 'publicities': publicities})
+
+    print(context)
     # return HttpResponse("Hello, world. You're at the site home.")
-    return render(request, 'site/index.html', {})
+    return render(request, 'site/index.html', context)
+
+
+def display_article(request, *args, **kwargs):
+    context = {}
+    article_id = kwargs["id"]
+    article = Article.objects.get(id=article_id)
+
+    context.update({'article': article})
+    return render(request, 'site/article_detail.html', context)
+
+
+def display_publicity(request, *args, **kwargs):
+    context = {}
+    publicity_id = kwargs["id"]
+    publicity = Publicity.objects.get(id=publicity_id)
+
+    context.update({'publicity': publicity})
+    return render(request, 'site/display_publicity.html', context)
