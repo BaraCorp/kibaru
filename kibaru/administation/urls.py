5c5ab6b74efb11c5323a2cf99c14d9d7a5d8ce82
Alou Dolo <aloud7@gmail.com>
2015-10-06 17:53:37 +0000

    suppression de home dans admin

--- kibaru/administation/urls.py
+++ kibaru/administation/urls.py
@@ -10,7 +10,7 @@ from kibaru.administation import views
 
 
 urlpatterns = patterns('',
-                       url(r'^home/$', views.home, name='admin_home'),
+                       url(r'^$', views.home, name='admin_home'),
                        url(r'^add_article/$', views.add_article, name='add_article'),
                        url(r'^edit_article/(?P<id>\d+)$', views.edit_article, name='edit_article'),
                        url(r'^del_article/(?P<id>\d+)$', views.del_article, name="del_article"),
