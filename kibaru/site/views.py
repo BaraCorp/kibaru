14ff7a543584d2d3591e4e68664c7acc7c0eede2
fadiga <ibfadiga@gmail.com>
2015-12-03 14:24:38 +0000

    fix bug on the link posts

--- kibaru/site/views.py
+++ kibaru/site/views.py
@@ -57,15 +57,15 @@ def init(month=None, year=None, cat_slug=None):
     elif year:
         posts = posts.filter(date_created__year=year)
 
-    for article in posts.iterator():
+    for article in posts:
         article.url_display = reverse("display_article", args=[article.slug])
         # article.intro = article.text.split('</p>')[0][3:]
-    for publicity in publicities.iterator():
+    for publicity in publicities:
         publicity.url_display = reverse(
             "display_publicity", args=[publicity.id])
 
     news = New.objects.order_by('-date')
-    for new in news.iterator():
+    for new in news:
         new.url_display = reverse("display_new", args=[new.id])
     if news.filter(date__gte=datetime.now()):
         news = news.filter(date__gte=datetime.now())
@@ -84,7 +84,7 @@ def create_archive_data(posts):
     archive_data = []
     count = {}
     mcount = {}
-    for post in posts.iterator():
+    for post in posts:
         year = post.date_created.year
         month = post.date_created.month
         if year not in count:
